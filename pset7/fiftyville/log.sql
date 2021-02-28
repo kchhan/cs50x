11943f53a481056b0e34fa09148919305eb1510f
Kevin Chhan <kvnchhan@gmail.com>
2021-02-28 03:10:40 +0000

    complete pset7

---
+++ pset7/fiftyville/log.sql
@@ -0,0 +1,13 @@
+-- Keep a log of any SQL queries you execute as you solve the mystery.
+
+Chamberlin Street | Theft of the CS50 duck took place at 10:15am at the Chamberlin Street courthouse. Interviews were conducted today with three witnesses who were present at the time — each of their interview transcripts mentions the courthouse.
+
+RUTH: Sometime within ten minutes of the theft, I saw the thief get into a car in the courthouse parking lot and drive away. If you have security footage from the courthouse parking lot, you might want to look for cars that left the parking lot in that time frame.
+EUGENE: I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at the courthouse, I was walking by the ATM on Fifer Street and saw the thief there withdrawing some money.
+RAYMOND: As the thief was leaving the courthouse, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket.
+
+select * from flights
+   ...> join airports 
+   ...> on flights.origin_airport_id = airports.id
+   ...> WHERE month = 7 and day = 29
+   ...> order by hour;
